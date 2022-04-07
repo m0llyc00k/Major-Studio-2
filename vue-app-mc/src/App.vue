42181ea1b3ffcce236a8e9500f15e374eb09b69e
m0llyc00k <86888346+m0llyc00k@users.noreply.github.com>
2022-04-07 18:46:56 -0400

    scrollytelling start

--- vue-app-mc/src/App.vue
+++ vue-app-mc/src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <div>
+  <div id="app">
+    <div class="intro">
+      <p>SCROLL TO CONTINUE</p>
+    </div>
     <h1>Opioid Timeline</h1>
-    <div class="recommendation-group"></div>
-    <arcTimeline :data="data" :height="2000" :width="1300" class="arc" />
-    <div id="chart"></div>
+    <div class="main">
+      <!-- <div class="main__graphic">{{ currStep }}</div> -->
+      <arcTimeline :data="data" :height="height" :width="width" class="arc" />
+
+      <Scrollama
+        :debug="true"
+        :offset="0.5"
+        @step-enter="({ element }) => (currStep = element.data.stepNo)"
+        class="main__scrollama"
+      >
+        <div class="step" data-step-no="1">step 1</div>
+        <div class="step" data-step-no="2">step 2</div>
+        <div class="step" data-step-no="3">step 3</div>
+        <div class="step" data-step-no="4">step 4</div>
+      </Scrollama>
+    </div>
+    <div class="outro">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, at velit
+      sint facere ipsam doloremque placeat vel impedit sapiente alias.
+    </div>
   </div>
 </template>
 
 <script>
 import arcTimeline from "./components/arcTimeline.vue";
-
+// import Scroll from "./components/Scroll.vue";
 import data from "../data.json";
+import "intersection-observer";
+// import Scrollama from "vue-scrollama";
+
+<style src="vue-scrollama/dist/vue-scrollama.css"></style>;
 
 export default {
   name: "App",
   components: {
     arcTimeline,
+    // Scroll,
+    // Scrollama,
   },
 
   data() {
     return {
       data: data,
+      currStep: 1,
+      height: "100%",
+      width: "100%",
     };
   },
   computed: {},
@@ -28,16 +57,95 @@ export default {
   mounted() {
     console.log("i am mounted");
   },
+  methods: {
+    stepEnterHandler({ element, index, direction }) {
+      // handle the step-event as required here
+      console.log({ element, index, direction });
+      // use the data attributes if needed
+      console.log(element.dataset.step); // a, b or c
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+.intro,
+.outro {
+  padding: 20vh;
+}
+
+/* // additions and overrides of DOM elements vue-scrollama sets up */
+.main {
+  display: flex;
+}
+.main__graphic {
+  flex: 1;
+  height: 80vh;
+  top: 10vh;
+  position: sticky;
+}
+.main__scrollama {
+  flex: 1;
+}
+.arc {
+  flex: 4;
+  height: 80vh;
+  top: 10vh;
+  position: sticky;
+}
+/* // your elements styles */
+/* .main__graphic {
+  height: 100%;
+  margin: 0 3rem;
+  border: 1px solid #ccc;
+  background-color: #eee;
+  font-size: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+} */
+
+.main__graphic {
+  position: sticky;
+  top: 20;
+  height: 50vh;
+  border: 1px solid #ccc;
+  background-color: #eee;
+  font-size: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+/* .step {
+  padding: 20vh 0;
+  margin: 0 3rem;
+  margin-bottom: 30vh;
+  background-color: beige;
+  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+} */
+.step {
+  padding: 15vh 0;
+  width: 50%;
+  margin: 0 auto 30vh;
+  background-color: beige;
+  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+:last-child {
+  margin-bottom: 0;
+}
 </style>
+
--- vue-app-mc/src/components/scroll.vue
+++ vue-app-mc/src/components/scroll.vue
@@ -1,30 +1,102 @@
 <template>
-  <Scrollama @step-enter="stepEnterHandler">
-    <div class="step-1" data-step="a">...</div>
-    // classes like .step-1 may be used to adjust the style and dimensions of a
-    step
-    <div class="step-2" data-step="b">...</div>
-    // data-* attributes can be helpful to store instructions to be used in
-    handlers
-    <div class="step-3" data-step="c">...</div>
-  </Scrollama>
+  <div>
+    <div class="intro">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, at
+        velit sint facere ipsam doloremque placeat vel impedit sapiente alias.
+      </p>
+      <p>SCROLL TO CONTINUE</p>
+    </div>
+
+    <div class="main">
+      <div class="main__graphic">{{ currStep }}</div>
+      <Scrollama
+        :debug="true"
+        :offset="0.5"
+        @step-enter="({ element }) => (currStep = element.dataset.stepNo)"
+        class="main__scrollama"
+      >
+        <div class="step" data-step-no="1">step 1</div>
+        <div class="step" data-step-no="2">step 2</div>
+        <div class="step" data-step-no="3">step 3</div>
+        <div class="step" data-step-no="4">step 4</div>
+      </Scrollama>
+    </div>
+
+    <div class="outro">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, at velit
+      sint facere ipsam doloremque placeat vel impedit sapiente alias.
+    </div>
+  </div>
 </template>
 
 <script>
-import "intersection-observer"; // for cross-browser support
-import Scrollama from "vue-scrollama"; // local registration in this example, can also be globally registered
+import "intersection-observer";
+import Scrollama from "vue-scrollama";
 
 export default {
+  name: "Scroll",
   components: {
-    Scrollama, // local registration in this example, can also be globally registered
+    Scrollama,
   },
-  methods: {
-    stepEnterHandler({ element, index, direction }) {
-      // handle the step-event as required here
-      console.log({ element, index, direction });
-      // use the data attributes if needed
-      console.log(element.dataset.step); // a, b or c
-    },
+  data() {
+    return {
+      currStep: null,
+    };
   },
 };
 </script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.intro,
+.outro {
+  padding: 20vh;
+}
+
+/* // additions and overrides of DOM elements vue-scrollama sets up */
+.main {
+  display: flex;
+}
+.main__graphic {
+  flex: 1;
+  height: 80vh;
+  top: 10vh;
+  position: sticky;
+}
+.main__scrollama {
+  flex: 1;
+}
+
+/* // your elements styles */
+.main__graphic {
+  height: 100%;
+  margin: 0 3rem;
+  border: 1px solid #ccc;
+  background-color: #eee;
+  font-size: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.step {
+  padding: 20vh 0;
+  margin: 0 3rem;
+  margin-bottom: 10vh;
+  background-color: beige;
+  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+:last-child {
+  margin-bottom: 0;
+}
+</style>
