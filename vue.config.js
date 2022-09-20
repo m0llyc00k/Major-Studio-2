d7e7b0fb3123fc1ce3a740ff855d464baf0e7835
m0llyc00k <mollyc628@gmail.com>
2022-09-19 22:18:07 -0400

    change public path

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,9 @@ module.exports = {
   devServer: {
     disableHostCheck: true
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Thesis-2022/'
-    : '/'
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/' + require('./package.json').name + '/'
+      : '/'
 }
 
